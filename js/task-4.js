d6494b97f1f1d8fc037ea1d972ce260e7b4243b0
Will IM <vitalii.mieliet@gmail.com>
2025-04-27 15:08:41 +0300

    Add 4 rask

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,22 @@
+const loginForm = document.querySelector('.login-form');
 
+loginForm.addEventListener('submit', event => {
+  event.preventDefault();
+  const { email, password } = loginForm.elements;
+  const trimmedEmail = email.value.trim();
+  const trimmedPassword = password.value.trim();
+
+  if (trimmedEmail === '' || trimmedPassword === '') {
+    alert('All form fields must be filled in');
+    return;
+  }
+
+  const formData = {
+    email: trimmedEmail,
+    password: trimmedPassword,
+  };
+
+  console.log(formData);
+
+  loginForm.reset();
+});
